9bfdd895642ebcc7375c31e4a2c214df7ff26796
肉鸡二号 <rookies_two@yeah.net>
2024-07-20 23:16:48 +0800

    bugfix

--- src/main/java/top/rookiestwo/wheatsync/screen/SLIScreenHandler.java
+++ src/main/java/top/rookiestwo/wheatsync/screen/SLIScreenHandler.java
@@ -94,12 +94,12 @@ public class SLIScreenHandler extends ScreenHandler {
 
     @Override
     public void onClosed(PlayerEntity player) {
-        if (player instanceof ServerPlayerEntity) {
+        if (player instanceof ServerPlayerEntity serverPlayer) {
             if (SLIEntity != null) {
                 if (SLIEntity.ifInventoryChanged()) {
                     WheatSync.sliCache.updateSLIInventory(SLIEntity.getBLOCK_PLACER(), SLIEntity.getCommunicationID(), SLICache.serializeInventory(SLIEntity.getInventory()));
-                    SLIEntity.clear();
                 }
+                SLIEntity.clear();
             }
         }
         super.onClosed(player);
